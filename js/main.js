47d456bc0243b076021d6182992029d1a7028ed4
tubi55 <tubi55@nate.com>
2022-01-07 22:51:21 +0900

    출처 표기

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('main');
 const loading = document.querySelector('aside');
+const logo = document.querySelector('p img');
 
 for(let i=0; i<200; i++){
     const imgNode = document.createElement('img');
@@ -11,6 +12,12 @@ imageLoaded();
 
 window.addEventListener('mousemove', e=>{
     let x = e.pageX;
+    let y = e.pageY;
+    let cx = -x/10;
+    let cy = -y/10;
+
+    logo.style.transform = `translate(${cx}px, ${cy}px)`;
+
     let wid = window.innerWidth;
     let percent = parseInt((x/wid)*200);
     console.log(percent);
