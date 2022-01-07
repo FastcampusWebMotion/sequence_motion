6a1d79d16d70d112ee2095b89b23e7c655e376dc
tubi55 <tubi55@nate.com>
2022-01-07 21:43:56 +0900

    모든 이미지 소스 로딩 완료후 화면 출력

--- js/main.js
+++ js/main.js
@@ -1,28 +1,22 @@
-/*
-    동적으로 DOM생성
-    부모요소.innerHTML= "집어넣을 태그를 문자열";
-    --> 기존 부모안쪽의 자식요소를 모두 제거하고 새로 생성
-
-    부모요소.append(DOM node);
-    --> 인수로 생성할 태그의 문자열이 아닌 노드를 생성해서 삽입
-
-    새로운 DOM노드 생성방법
-    document.createElement('dom이름')
-*/
 const main = document.querySelector('main');
 
-/*
 for(let i=0; i<200; i++){
     const imgNode = document.createElement('img');
     imgNode.setAttribute('src', `img/pic${i}.jpg`);
     main.append(imgNode);
 }
-*/
-let tags = '';
-for(let i=0; i<200; i++){
-    tags += `<img src='img/pic${i}.jpg' />`;
-}
 
-console.log(tags);
-main.innerHTML = tags;
+const imgs = document.querySelectorAll('img');
+const len = imgs.length;
+let total = 0;
+
+imgs.forEach(img=>{
+    img.addEventListener('load',()=>{
+        total++;
+        console.log(total);
+
+        if(total === len) main.classList.add('on');
+    })
+})
+
 
